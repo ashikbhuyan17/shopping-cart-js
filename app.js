199bcc716c01bb3c96bd8d2932a890fefebcf6b4
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-01-29 03:45:13 +0600

    added function and done full project

--- app.js
+++ app.js
@@ -1,49 +1,175 @@
+// phone price
+
+
+// document.getElementById('buttonClick').addEventListener('click', function () {
+
+//     let currentValue = Number(document.getElementById('increaseValue').value)
+//     currentValue++;
+//     console.log(typeof currentValue, currentValue);
+//     // increase value
+//     document.getElementById('increaseValue').value = currentValue
+
+//     // increase total amount
+//     let currentAmount = Number(document.getElementById('updateValue').innerText)
+//     let totalAmount = 1219 * currentValue
+//     console.log(typeof currentAmount, currentAmount, totalAmount);
+//     document.getElementById('updateValue').innerText = totalAmount
+// })
+// document.getElementById('decreaseValue').addEventListener('click', function () {
+//     let currentValue = Number(document.getElementById('increaseValue').value)
+//     currentValue--;
+//     if (currentValue < 0) {
+//         currentValue = 0
+//     }
+//     console.log(typeof currentValue);
+//     document.getElementById('increaseValue').value = currentValue
+
+//     // decrease total amount
+//     let currentAmount = Number(document.getElementById('updateValue').innerText)
+//     let totalAmount = 1219 * currentValue
+//     console.log(typeof currentAmount, currentAmount, totalAmount);
+//     document.getElementById('updateValue').innerText = totalAmount
+
+// })
+
+
+// // item increasement (Case - Black)
+
+// document.getElementById('CaseBlackIncrease').addEventListener('click', function () {
+
+//     let currentValue = Number(document.getElementById('CaseBlackIncreaseValue').value)
+//     currentValue++;
+//     console.log(typeof currentValue, currentValue);
+//     // increase value
+//     document.getElementById('CaseBlackIncreaseValue').value = currentValue
+
+//     // increase total amount
+//     let currentAmount = Number(document.getElementById('CaseBlackUpdateValue').innerText)
+//     let totalAmount = 50 * currentValue
+//     console.log(typeof currentAmount, currentAmount, totalAmount);
+//     document.getElementById('CaseBlackUpdateValue').innerText = totalAmount
+
+
+// })
+
+
+// // item Decreasement (Case - Black)
+
+// document.getElementById('CaseBlackDecreaseValue').addEventListener('click', function () {
+//     let currentValue = Number(document.getElementById('CaseBlackIncreaseValue').value)
+//     currentValue--;
+//     if (currentValue < 0) {
+//         currentValue = 0
+//     }
+//     console.log(typeof currentValue);
+//     document.getElementById('CaseBlackIncreaseValue').value = currentValue
+
+//     // decrease total amount
+//     let currentAmount = Number(document.getElementById('CaseBlackUpdateValue').innerText)
+//     let totalAmount = 50 * currentValue
+//     console.log(typeof currentAmount, currentAmount, totalAmount);
+//     document.getElementById('CaseBlackUpdateValue').innerText = totalAmount
+
+// })
+
+
+
+//  phone price
+
 document.getElementById('buttonClick').addEventListener('click', function () {
 
-    let currentValue = Number(document.getElementById('increaseValue').value)
-    currentValue++;
-    console.log(typeof currentValue, currentValue);
-    // increase value
-    document.getElementById('increaseValue').value = currentValue
+    // let currentValue = Number(document.getElementById('increaseValue').value)
+    // currentValue++;
+    // // console.log(typeof currentValue, currentValue);
+    // // increase value
+    // document.getElementById('increaseValue').value = currentValue
 
-    // increase total amount
-    let currentAmount = Number(document.getElementById('updateValue').innerText)
-    let totalAmount = 1219 * currentValue
-    console.log(typeof currentAmount, currentAmount, totalAmount);
-    document.getElementById('updateValue').innerText = totalAmount
+    // // increase total amount
+    // let currentAmount = Number(document.getElementById('updateValue').innerText)
+    // let totalAmount = 1219 * currentValue
+    // console.log(typeof currentAmount, currentAmount, totalAmount);
+    // document.getElementById('updateValue').innerText = totalAmount
+
+    handleProductChange(true);
+
 })
+
 document.getElementById('decreaseValue').addEventListener('click', function () {
     let currentValue = Number(document.getElementById('increaseValue').value)
-    currentValue--;
-    if (currentValue < 0) {
-        currentValue = 0
-    }
-    console.log(typeof currentValue);
-    document.getElementById('increaseValue').value = currentValue
+    // currentValue--;
+    // if (currentValue < 0) {
+    //     currentValue = 0
+    // }
 
-    // decrease total amount
+    // console.log(typeof currentValue);
+    // document.getElementById('increaseValue').value = currentValue
+
+    // // decrease total amount
+    // let currentAmount = Number(document.getElementById('updateValue').innerText)
+    // let totalAmount = 1219 * currentValue
+    // console.log(typeof currentAmount, currentAmount, totalAmount);
+    // document.getElementById('updateValue').innerText = totalAmount
+
+    handleProductChange(false);
+
+});
+
+function handleProductChange(isIncrease) {
+    let currentValue = Number(document.getElementById('increaseValue').value)
+
+    if (isIncrease == true) {
+        currentValue++;
+    } else if (isIncrease == false & currentValue > 0) {
+        currentValue--;
+    }
+    document.getElementById('increaseValue').value = currentValue
     let currentAmount = Number(document.getElementById('updateValue').innerText)
     let totalAmount = 1219 * currentValue
     console.log(typeof currentAmount, currentAmount, totalAmount);
     document.getElementById('updateValue').innerText = totalAmount
+}
 
-})
+
+
+
+
+// case black
+
+function handleCaseBlack(isIncrease) {
+    let currentValue = Number(document.getElementById('CaseBlackIncreaseValue').value)
+
+    if (isIncrease == true) {
+        currentValue++;
+    } else if (isIncrease == false & currentValue > 0) {
+        currentValue--;
+    }
+    document.getElementById('CaseBlackIncreaseValue').value = currentValue
+    let currentAmount = Number(document.getElementById('CaseBlackUpdateValue').innerText)
+    let totalAmount = 50 * currentValue
+    console.log(typeof currentAmount, currentAmount, totalAmount);
+    document.getElementById('CaseBlackUpdateValue').innerText = totalAmount
+}
+
+
+
 
 // item increasement (Case - Black)
 
 document.getElementById('CaseBlackIncrease').addEventListener('click', function () {
 
-    let currentValue = Number(document.getElementById('CaseBlackIncreaseValue').value)
-    currentValue++;
-    console.log(typeof currentValue, currentValue);
-    // increase value
-    document.getElementById('CaseBlackIncreaseValue').value = currentValue
+    // let currentValue = Number(document.getElementById('CaseBlackIncreaseValue').value)
+    // currentValue++;
+    // console.log(typeof currentValue, currentValue);
+    // // increase value
+    // document.getElementById('CaseBlackIncreaseValue').value = currentValue
 
-    // increase total amount
-    let currentAmount = Number(document.getElementById('CaseBlackUpdateValue').innerText)
-    let totalAmount = 50 * currentValue
-    console.log(typeof currentAmount, currentAmount, totalAmount);
-    document.getElementById('CaseBlackUpdateValue').innerText = totalAmount
+    // // increase total amount
+    // let currentAmount = Number(document.getElementById('CaseBlackUpdateValue').innerText)
+    // let totalAmount = 50 * currentValue
+    // console.log(typeof currentAmount, currentAmount, totalAmount);
+    // document.getElementById('CaseBlackUpdateValue').innerText = totalAmount
+
+    handleCaseBlack(true);
 
 
 })
@@ -52,21 +178,23 @@ document.getElementById('CaseBlackIncrease').addEventListener('click', function 
 // item Decreasement (Case - Black)
 
 document.getElementById('CaseBlackDecreaseValue').addEventListener('click', function () {
-    let currentValue = Number(document.getElementById('CaseBlackIncreaseValue').value)
-    currentValue--;
-    if (currentValue < 0) {
-        currentValue = 0
-    }
-    console.log(typeof currentValue);
-    document.getElementById('CaseBlackIncreaseValue').value = currentValue
+    // let currentValue = Number(document.getElementById('CaseBlackIncreaseValue').value)
+    // currentValue--;
+    // if (currentValue < 0) {
+    //     currentValue = 0
+    // }
+    // console.log(typeof currentValue);
+    // document.getElementById('CaseBlackIncreaseValue').value = currentValue
 
-    // decrease total amount
-    let currentAmount = Number(document.getElementById('CaseBlackUpdateValue').innerText)
-    let totalAmount = 50 * currentValue
-    console.log(typeof currentAmount, currentAmount, totalAmount);
-    document.getElementById('CaseBlackUpdateValue').innerText = totalAmount
+    // // decrease total amount
+    // let currentAmount = Number(document.getElementById('CaseBlackUpdateValue').innerText)
+    // let totalAmount = 50 * currentValue
+    // console.log(typeof currentAmount, currentAmount, totalAmount);
+    // document.getElementById('CaseBlackUpdateValue').innerText = totalAmount
 
-})
+    handleCaseBlack(false);
+
+});
 
 
 
---
+++ app02.js
@@ -0,0 +1,156 @@
+// document.getElementById('buttonClick').addEventListener('click', function () {
+
+//     // let productInput = Number(document.getElementById('increaseValue').value)
+//     // productInput++;
+//     // // console.log(typeof productInput, productInput);
+//     // // increase value
+//     // document.getElementById('increaseValue').value = productInput
+
+//     // // increase total amount
+//     // let currentAmount = Number(document.getElementById('updateValue').innerText)
+//     // let totalAmount = 1219 * productInput
+//     // console.log(typeof currentAmount, currentAmount, totalAmount);
+//     // document.getElementById('updateValue').innerText = totalAmount
+
+//     handleProductChange(true);
+
+// })
+
+// document.getElementById('decreaseValue').addEventListener('click', function () {
+//     let productInput = Number(document.getElementById('increaseValue').value)
+//     // productInput--;
+//     // if (productInput < 0) {
+//     //     productInput = 0
+//     // }
+
+//     // console.log(typeof productInput);
+//     // document.getElementById('increaseValue').value = productInput
+
+//     // // decrease total amount
+//     // let currentAmount = Number(document.getElementById('updateValue').innerText)
+//     // let totalAmount = 1219 * productInput
+//     // console.log(typeof currentAmount, currentAmount, totalAmount);
+//     // document.getElementById('updateValue').innerText = totalAmount
+
+//     handleProductChange(false);
+
+// });
+
+
+
+
+/** 
+function handleProductChange(isIncrease) {
+    let productInput = Number(document.getElementById('phone-count').value)
+
+    if (isIncrease == true) {
+        productInput++;
+    } else if (isIncrease == false & productInput > 0) {
+        productInput--;
+    }
+    document.getElementById('phone-count').value = productInput
+    let currentAmount = Number(document.getElementById('updateValue').innerText)
+    let totalAmount = 1219 * productInput
+    console.log(typeof currentAmount, currentAmount, totalAmount);
+    document.getElementById('updateValue').innerText = totalAmount
+}
+
+function handleCaseBlack(isIncrease) {
+    let productInput = Number(document.getElementById('case-count').value)
+
+    if (isIncrease == true) {
+        productInput++;
+    } else if (isIncrease == false & productInput > 0) {
+        productInput--;
+    }
+    document.getElementById('case-count').value = productInput
+    let currentAmount = Number(document.getElementById('CaseBlackUpdateValue').innerText)
+    let totalAmount = 50 * productInput
+    console.log(typeof currentAmount, currentAmount, totalAmount);
+    document.getElementById('CaseBlackUpdateValue').innerText = totalAmount
+}
+*/
+
+function handleCaseBlack(product, isIncrease) {
+    let productInput = Number(document.getElementById(product + '-count').value)
+
+    if (isIncrease == true) {
+        productInput++;
+    } else if (isIncrease == false & productInput > 0) {
+        productInput--;
+    }
+    document.getElementById(product + '-count').value = productInput
+    let currentAmount = Number(document.getElementById(product).innerText)
+    let productTotal = 0
+    // let totalAmount = 50 * productInput
+    if (product == 'phone') {
+        productTotal = 1200 * productInput
+    }
+    if (product == 'case') {
+        productTotal = 50 * productInput
+    }
+    console.log(typeof currentAmount, currentAmount, productTotal);
+    document.getElementById(product).innerText = productTotal
+    calculateTotal();
+
+}
+
+function calculateTotal() {
+    const phoneInput = Number(document.getElementById('phone-count').value)
+    const caseInput = Number(document.getElementById('case-count').value)
+    const totalAmount = phoneInput * 1200 + caseInput * 50
+    document.getElementById('total-price').innerText = '$' + totalAmount
+
+}
+
+
+
+// item increasement (Case - Black)
+
+// document.getElementById('CaseBlackIncrease').addEventListener('click', function () {
+
+//     // let productInput = Number(document.getElementById('CaseBlackIncreaseValue').value)
+//     // productInput++;
+//     // console.log(typeof productInput, productInput);
+//     // // increase value
+//     // document.getElementById('CaseBlackIncreaseValue').value = productInput
+
+//     // // increase total amount
+//     // let currentAmount = Number(document.getElementById('CaseBlackUpdateValue').innerText)
+//     // let totalAmount = 50 * productInput
+//     // console.log(typeof currentAmount, currentAmount, totalAmount);
+//     // document.getElementById('CaseBlackUpdateValue').innerText = totalAmount
+
+//     handleCaseBlack(true);
+
+
+// })
+
+
+// // item Decreasement (Case - Black)
+
+// document.getElementById('CaseBlackDecreaseValue').addEventListener('click', function () {
+//     // let productInput = Number(document.getElementById('CaseBlackIncreaseValue').value)
+//     // productInput--;
+//     // if (productInput < 0) {
+//     //     productInput = 0
+//     // }
+//     // console.log(typeof productInput);
+//     // document.getElementById('CaseBlackIncreaseValue').value = productInput
+
+//     // // decrease total amount
+//     // let currentAmount = Number(document.getElementById('CaseBlackUpdateValue').innerText)
+//     // let totalAmount = 50 * productInput
+//     // console.log(typeof currentAmount, currentAmount, totalAmount);
+//     // document.getElementById('CaseBlackUpdateValue').innerText = totalAmount
+
+//     handleCaseBlack(false);
+
+// });
+
+
+
+
+
+
+
